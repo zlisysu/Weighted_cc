9f9e46cea89cf3a30615e89d43009b44c1c7a9c5
zlisysu <70196077+zlisysu@users.noreply.github.com>
2022-05-23 11:03:39 +0800

    Add files via upload

---
+++ CalLig.py
@@ -0,0 +1,76 @@
+import sys
+import decimal
+import numpy as np
+from Graphs import  Graph
+decimal.getcontext().rounding = "ROUND_HALF_UP"
+
+def set_node_map(graph):
+    node_map = [[decimal.Decimal(sys.maxsize) if (val1 != val2) else decimal.Decimal(0.0) for val2 in range(len(graph.V))]
+            for val1 in range(len(graph.V))]
+    for x, y, val in graph.nodelist:
+        node_map[graph.V.index(x)][graph.V.index(y)] = node_map[graph.V.index(y)][graph.V.index(x)] = decimal.Decimal(val ** 2)
+    return  node_map
+
+def cal_node_noref_error(node,node_map):
+    node_rel=[[decimal.Decimal(-1) if (p==sys.maxsize) else p for p in n]for n in node_map]
+    no_ref_error=[np.max(n) for n in node_rel]
+    return no_ref_error
+
+def cal_node_ref_error(source,node,node_map):
+    vertex_num=len(node)
+    visited=[False for i in range(vertex_num)]
+    visited[source]=True;
+    dist=[0 for i in range(vertex_num)]
+    pre = [0 for i in range(vertex_num)]
+    for i in range(len(node)):
+        dist[i]=node_map[source][i];
+        pre[i]=source;
+    for i in range(1,vertex_num):
+        min_cost=sys.maxsize
+        for j in range(vertex_num):
+            if visited[j]==False and dist[j]<min_cost:
+                min_cost_index=j
+                min_cost=dist[j]
+        visited[min_cost_index]=True
+        for j in range(vertex_num):
+            if visited[j]==False and node_map[min_cost_index][j] != sys.maxsize and min_cost+node_map[min_cost_index][j]<dist[j]:
+                dist[j]=min_cost+node_map[min_cost_index][j]
+                pre[j]= min_cost_index
+
+    path_all=[]
+    for i in range(0,vertex_num):
+        path=[]
+        path.append(i)
+        if i!=source :
+            t=pre[i]
+            path.append(t)
+            while t!=source:
+                t=pre[t]
+                path.append(t)
+        path_all.append(path)
+
+    return dist,path_all
+
+def calcMolEnes(ref_ene,graph,path):
+    mol_ene=[[ref_ene for i in range(len(graph.V))] for j in range(0,graph.weight_num)]
+    for k in range(0,graph.weight_num):
+        for m in range(len(graph.V)):
+            route_mol_list = path[m]
+            for i in range(len(route_mol_list) - 1):
+                curr_path = (graph.V[route_mol_list[i]], graph.V[route_mol_list[i + 1]])
+                curr_ene = graph.ddG_cc[curr_path][k]
+                mol_ene[k][m] -= float(curr_ene)###
+    return mol_ene
+
+def printMol(nodes,mol_ene,node_err,no_ref_error):
+
+    print("*"*100)
+    print('{:^4s} {:^12s}'.format('Node', 'dG_cc'), end='')
+    for k in range(1,len(mol_ene)):
+        print(' {:^12s}'.format("dG_wcc" + str(k)), end='')
+    print(' {:^12s} {:^12s}'.format('cc_ref_error','no_ref_error'))
+    for i in range(len(nodes)):
+        print("{:^4s} {:^12.4f}".format(nodes[i],mol_ene[0][i]),end='')
+        for k in range(1, len(mol_ene)):
+            print(' {:^12.4f}'.format(mol_ene[k][i]), end='')
+        print(' {:^12.4f} {:^12.4f}'.format(node_err[i].sqrt().quantize(decimal.Decimal('0.00')),no_ref_error[i]))
---
+++ wcc_main.py
@@ -0,0 +1,40 @@
+from optparse import OptionParser
+from Graphs import Graph
+import  CalLig as lig
+
+
+#if weights contained bennett_std, pleas display bennett_std before other weights
+class optParser():
+    def __init__(self, fakeArgs):
+        parser = OptionParser()
+        parser.add_option('-f', '--file', dest='file', help='Input file containing pairwise energy data', default='thrombin.txt')
+        parser.add_option('-r', '--ref', dest='ref',help='Reference molecule for calculating the energy for other molecules. Default: The first molecule in the data file',
+                          default='')
+        parser.add_option('-e', '--ref_ene', dest='ref_ene', help='Energy for the reference molecule. Default: 0.00',
+                          default=0.00, type=float)
+        parser.add_option('-w', '--weight', dest='weight', help='Weight option: no(original cycle closure), bar(if weights contained bennett_std). Default: 0',
+                          default="no")
+        if fakeArgs:
+            self.option, self.args = parser.parse_args(fakeArgs)
+        else:
+            self.option, self.args = parser.parse_args()
+
+if __name__ == '__main__':
+    #opts = optParser('')
+    fakeArgs = "-f bace_run1_0_with_w -r 3A -e -8.83 -w bar"  # only keep this for test purpose
+    opts = optParser(fakeArgs.strip().split())  # only keep this for test purpose
+    g = Graph(opts.option.file, opts.option.weight)
+    g.getAllCyles()
+    if len(g.cycles)== 0:
+        print("No cycle in this graph.")
+        exit()
+    g.iterateCycleClosure(minimum_cycles=2)
+    g.printEnePairs()
+    node_map=lig.set_node_map(g)
+    ref_node = g.V.index(opts.option.ref)
+    no_ref_error = lig.cal_node_noref_error(g.V, node_map)
+    ref_node_err, path = lig.cal_node_ref_error(ref_node, g.V, node_map)
+    mol_ene = lig.calcMolEnes(opts.option.ref_ene, g, path)
+    lig.printMol(g.V,mol_ene,ref_node_err,no_ref_error)
+
+
